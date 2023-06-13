7ae0660029325fe6d80d2b3ca886392c3fc877cb
arsalan-yousuf <arsalan98y@gmail.com>
2023-06-13 11:28:55 +0500

    first push

--- Fixer/Controllers/ServiceController.cs
+++ Fixer/Controllers/ServiceController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fixer.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fixer.Controllers
 {
     public class ServiceController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Service service)
         {
             return View();
         }
